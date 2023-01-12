b16362b8c233996dc99e44627a32e60f171d6f48
Christophe Delord <cdelord.fr>
2023-01-12 17:14:36 +0100

    panda cache creation

--- makex.mk
+++ makex.mk
@@ -355,10 +355,10 @@ export PATH := $(dir $(PANDA)):$(PATH)
 
 export PANDA_CACHE ?= $(MAKEX_CACHE)/.panda
 
-$(dir $(PANDA)):
+$(dir $(PANDA)) $(PANDA_CACHE):
 	@mkdir -p $@
 
-$(PANDA): | $(PANDOC) $(MAKEX_CACHE) $(dir $(PANDA))
+$(PANDA): | $(PANDOC) $(MAKEX_CACHE) $(dir $(PANDA)) $(PANDA_CACHE)
 	@echo "$(MAKEX_COLOR)[MAKEX]$(NORMAL) $(TEXT_COLOR)install Panda$(NORMAL)"
 	@test -f $(@) \
 	|| \
